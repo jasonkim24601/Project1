b4039527e0c8016c006c69d311e757f60e6c5337
jasonkim24601 <104212407+jasonkim24601@users.noreply.github.com>
2022-05-12 17:15:26 -0500

    Renamed the window

--- gui.py
+++ gui.py
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("Rock Paper Scissors!")
         MainWindow.resize(400, 400)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
